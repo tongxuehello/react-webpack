62c93cb55ed4f0a7839bbf3cdd87975c92717856
shaohuimeng <huimengshao@qq.com>
2016-04-06 18:01:41 +0800

    修复publicPath设置错误的bug

--- js/Hello2.js
+++ js/Hello2.js
@@ -3,7 +3,7 @@ var Hello2 = React.createClass({
 	render() {
 		return (
 			<h1>
-				<span style={{color:"red",fontWeight:"bold"}}>Hello test shao hello ==1 </span> 
+				<span style={{color:"red",fontWeight:"bold"}}>Hello test shao hello ==2 </span> 
 				{this.props.name}!
 			</h1>
 		)
--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
 	],
 	output: {
 		path: __dirname + '/build/js/',
-		publicPath: "http://localhost:3000/",
+		publicPath: "http://localhost:3000/" + '/build/js/',
 		filename: 'bundle.js'
 	},
 	module: {
