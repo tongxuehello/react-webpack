0fe4b6558af05fb73e7b76cfa86d32250fc5dc1e
shaohuimeng <shaohuimeng@qiyi.com>
2016-05-03 22:10:31 +0800

    将webpack开发服务器的启动放在package.js中，只使用命令npm start即可启动开发服务器。

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,13 @@ const path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-	entry:[
-		'webpack-dev-server/client?http://localhost:3000', // WebpackDevServer host and port
+	entry:[ // 入口文件
+		'webpack-dev-server/client?http://localhost:3000', // webpack开发服务器，WebpackDevServer host and port
 		'webpack/hot/only-dev-server',
 		'./js/entry.js'
 	],
 	output: {
-		path: __dirname + '/build/js/',
+		path: __dirname + '/build/js/', // __dirname变量获取当前模块文件所在目录的完整绝对路径
 		publicPath: "http://localhost:3000/" + '/build/js/',
 		filename: 'bundle.js'
 	},
@@ -22,7 +22,7 @@ module.exports = {
 		*/
 		loaders: [{
 			test: /\.js$/,
-			exclude: path.resolve(__dirname, 'node_modules'),
+			exclude: path.resolve(__dirname, 'node_modules'), // 相对于运行Node.js程序的工作目录
 			include: [path.join(__dirname, 'js')],
 			loader: 'react-hot!babel-loader!jsx-loader?harmony'
 		}, {
